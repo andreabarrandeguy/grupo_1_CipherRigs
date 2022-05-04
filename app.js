256e4feff84578c231aff734ef7d4e825e5e9ec1
albruno-dev <alansergiobruno@gmail.com>
2022-05-04 06:41:09 -0300

    Reorganización y actualización de assets

--- app.js
+++ app.js
@@ -2,42 +2,44 @@ const express=require('express');
 const app=express();
 const path=require('path');
 
+app.use(express.static('public'));
 app.use(express.static('views'));
 
+
 app.get('/', (req, res) => { // homepage
-    res.status(200).sendFile(path.join(__dirname, 'views/index.html'));
+    res.status(200).sendFile(path.join(__dirname, '/src/views/index.html'));
 });
 
 app.get('/productos', (req, res) => { // Pagina de productos
-    res.status(200).sendFile(path.join(__dirname, 'views/productDetail.html'));
+    res.status(200).sendFile(path.join(__dirname, '/src/views/products/productDetail.html'));
 });
 
 app.get('/carrito', (req, res) => { // Pagina del carro
-    res.status(200).sendFile(path.join(__dirname, 'views/productCart.html'));
+    res.status(200).sendFile(path.join(__dirname, '/src/views/products/productCart.html'));
 });
 
 app.get('/loguearse', (req, res) => { // Pagina de logeo
-    res.status(200).sendFile(path.join(__dirname, 'views/login.html'));
+    res.status(200).sendFile(path.join(__dirname, '/src/views/users/login.html'));
 });
 
 app.get('/register', (req, res) => { // Pagina de registro
-    res.status(200).sendFile(path.join(__dirname, 'views/register.html'));
+    res.status(200).sendFile(path.join(__dirname, '/src/views/users/register.html'));
 });
 
 app.get('/nosotros', (req, res) => { // Pagina de registro
-    res.status(200).sendFile(path.join(__dirname, 'views/nosotros.html'));
+    res.status(200).sendFile(path.join(__dirname, '/src/views/nosotros.html'));
 });
 
 app.get('/contacto', (req, res) => { // Pagina de registro
-    res.status(200).sendFile(path.join(__dirname, 'views/contacto.html'));
+    res.status(200).sendFile(path.join(__dirname, '/src/views/contacto.html'));
 });
 
 app.get('/asic', (req, res) => { // Pagina de registro
-    res.status(200).sendFile(path.join(__dirname, 'views/asic.html'));
+    res.status(200).sendFile(path.join(__dirname, 'src/views/products/asic.html'));
 });
 
 app.get('/rigs', (req, res) => { // Pagina de registro
-     res.status(200).sendFile(path.join(__dirname, 'views/rigs.html'));
+     res.status(200).sendFile(path.join(__dirname, '/src/views/products/rigs.html'));
 });
 
 app.listen(3000, () => {
